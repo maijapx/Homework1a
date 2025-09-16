a0491d52a1e45a2a639c9a2ba5d4c1ac553ce88a
maijapx <ampaxman@byu.edu>
2025-09-15 21:48:22 -0600

    small edits

--- numbers_divisible_by.cpp
+++ numbers_divisible_by.cpp
@@ -4,19 +4,22 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-        int i = 10;
-        while (i >= 0) {
+    int i = 10;
+    while (i >= 0) {
         i --;
-        }
+    }
 
 
-int start = stod(argv[1]);
-int end = stod(argv[2]);
-int div = end / start;
+    double start = stod(argv[1]);
+    double end = stod(argv[2]);
+    int div = end / start;
 
-  if (start < end ) {}
-  if (start >= end ) {}
+    if (start < end ) {
+    for (double i = start
+
+    }
+    if (start >= end ) {}
 
 
-return 0;
+    return 0;
 }
