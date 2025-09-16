579749b3b640f3f6013f5454b10dad2bb5a661d8
maijapx <162735271+maijapx@users.noreply.github.com>
2025-09-15 21:19:58 -0600

    push for mac mini lol

--- numbers_divisible_by.cpp
+++ numbers_divisible_by.cpp
@@ -1,2 +1,22 @@
 // :D
+#include <iostream> //import
+using namespace std;
 
+int main(int argc, char *argv[]) {
+
+        int i = 10;
+        while (i >= 0) {
+        i --;
+        }
+
+
+int start = stod(argv[1]);
+int end = stod(argv[2]);
+int div = end / start;
+
+  if (start < end ) {}
+  if (start >= end ) {}
+
+
+return 0;
+}
