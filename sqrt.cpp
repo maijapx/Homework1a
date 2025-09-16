cebca32e8a62a89aff97a081276cd6f0d1b5d173
maijapx <162735271+maijapx@users.noreply.github.com>
2025-09-16 00:17:10 -0600

    sqrt completed

--- sqrt.cpp
+++ sqrt.cpp
@@ -1,6 +1,34 @@
 #include <cstdlib>  // atof
-using std::abs;  
+#include <iomanip>
+#include <iostream>
+using std::abs;
+using namespace std;
 
 const double EPSILON = 0.0001;
 
-// :)
+int main (int argc, char *argv[]) {
+    double i = stod (argv[1]);
+
+    if (i < 0){
+    cout << "Input must be greater than 0" << endl;
+    return 1;
+    }
+    double low = 0;
+    double high = i;
+    double mid;
+
+    cout << fixed << setprecision(5);
+
+    while (true){
+        mid = (low + high) / 2;
+        double square = mid * mid;
+        cout << mid << " (" << square << ")" << endl;
+        if (abs (square - i) < EPSILON) break;
+        if (square > i){
+            high = mid;
+        }else{
+            low = mid;
+        }
+    }
+    return 0;
+}
